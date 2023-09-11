0f8c67bebb2197848968052ed9bd03123ac8fb40
loinq2704 <nguyenquangloi2704@gmail.com>
2023-09-11 14:42:24 +0700

    First commit

---
+++ src/main/java/fu/swp/dorm_mnm/DormMnmApplication.java
@@ -0,0 +1,13 @@
+package fu.swp.dorm_mnm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DormMnmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DormMnmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fu/swp/dorm_mnm/DormMnmApplicationTests.java
@@ -0,0 +1,13 @@
+package fu.swp.dorm_mnm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DormMnmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
