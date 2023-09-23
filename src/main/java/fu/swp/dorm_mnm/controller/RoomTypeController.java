a165863bf31b9b243632fcf5ccd8d1a7638ec376
loinq2704 <nguyenquangloi2704@gmail.com>
2023-09-23 13:37:21 +0700

    add secu

--- src/main/java/fu/swp/dorm_mnm/controller/RoomTypeController.java
+++ src/main/java/fu/swp/dorm_mnm/controller/RoomTypeController.java
@@ -1,7 +1,6 @@
 package fu.swp.dorm_mnm.controller;
 
 
-import fu.swp.dorm_mnm.model.Room;
 import fu.swp.dorm_mnm.model.RoomType;
 import fu.swp.dorm_mnm.repository.RoomTypeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
