fb099959d0cd8dd4269b0294f66ae904ac20bb64
enricVan <vanminhtuan2003@gmail.com>
2023-09-25 15:08:46 +0700

    update RoomTypeController

--- src/main/java/fu/swp/dorm_mnm/service/RoomTypeService.java
+++ src/main/java/fu/swp/dorm_mnm/service/RoomTypeService.java
@@ -1,9 +1,18 @@
 package fu.swp.dorm_mnm.service;
 
 
+import fu.swp.dorm_mnm.model.Role;
 import fu.swp.dorm_mnm.model.RoomType;
+
+import java.util.Optional;
 
 public interface RoomTypeService {
     //CRUD
-    void save(RoomType roomType);
+    Iterable<RoomType> findAll();
+
+    Optional<RoomType> findById(Long id);
+
+    RoomType save(RoomType roomType);
+
+    void remove(Long id);
 }
--- src/main/java/fu/swp/dorm_mnm/service/serviceImpl/RoomTypeServiceImpl.java
+++ src/main/java/fu/swp/dorm_mnm/service/serviceImpl/RoomTypeServiceImpl.java
@@ -6,6 +6,8 @@ import fu.swp.dorm_mnm.service.RoomTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class RoomTypeServiceImpl implements RoomTypeService {
 
@@ -13,7 +15,23 @@ public class RoomTypeServiceImpl implements RoomTypeService {
     private RoomTypeRepository roomTypeRepository;
 
     @Override
-    public void save(RoomType roomType) {
-        roomTypeRepository.save(roomType);
+    public Iterable<RoomType> findAll() {
+        return roomTypeRepository.findAll();
     }
+
+    @Override
+    public Optional<RoomType> findById(Long id) {
+        return roomTypeRepository.findById(id);
+    }
+
+    @Override
+    public void remove(Long id) {
+        roomTypeRepository.deleteById(id);
+    }
+
+    @Override
+    public RoomType save(RoomType roomType) {
+        return roomTypeRepository.save(roomType);
+    }
+
 }
