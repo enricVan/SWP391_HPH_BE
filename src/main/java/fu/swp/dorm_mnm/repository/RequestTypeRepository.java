49d8ad449b281c78c18825322b240a6f5ddc9248
enricVan <vanminhtuan2003@gmail.com>
2023-09-25 17:34:04 +0700

    m

---
+++ src/main/java/fu/swp/dorm_mnm/repository/RequestTypeRepository.java
@@ -0,0 +1,7 @@
+package fu.swp.dorm_mnm.repository;
+
+import fu.swp.dorm_mnm.model.RequestType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RequestTypeRepository extends JpaRepository<RequestType, Long> {
+}
