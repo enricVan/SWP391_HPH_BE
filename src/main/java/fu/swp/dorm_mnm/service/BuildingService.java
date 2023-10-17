a520984bb66334ab4a34c893ad7202f20b1588fe
vhLong1083 <vuhoanglonghd2003@gmail.com>
2023-10-18 02:33:19 +0700

    AdminBuilding

---
+++ src/main/java/fu/swp/dorm_mnm/service/BuildingService.java
@@ -0,0 +1,16 @@
+package fu.swp.dorm_mnm.service;
+
+import fu.swp.dorm_mnm.model.Building;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BuildingService {
+    List<Building> findAll();
+
+    Optional<Building> findById(Long id);
+
+    Building save(Building building);
+
+    void remove(Long id);
+}
