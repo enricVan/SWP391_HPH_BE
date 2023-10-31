58e13f84dbf55933d4d13726d2369ffdcdde04e7
haikn <williamkieu2003@gmail.com>
2023-10-31 09:37:43 +0700

    userdto

--- src/main/java/fu/swp/dorm_mnm/vnpay/VnPayConstant.java
+++ src/main/java/fu/swp/dorm_mnm/vnpay/VnPayConstant.java
@@ -1,29 +1,32 @@
 package fu.swp.dorm_mnm.vnpay;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class VnPayConstant {
 
     public static String vnp_Version = "2.1.0";
     public static String vnp_Command = "2.1.0";
-    public static String vnp_TmnCode = "1X6BJ2DP";
 
-    public static String vnp_HashSecret = "FPNCXXTTAODDGNFKXPRVIMOKGUVGVZTC";
+    @Value("tmn-code")
+    public static String vnp_TmnCode;
+
+    @Value("{hash-secret}")
+    public static String vnp_HashSecret;
 
     public static String vnp_Url = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
     public static String vnp_BankCode = "NCB";
+
     public static String vnp_CurrCode = "VND";
-    //    public static String vnp_IpAddr = "0:0:0:0:0:0:0:1";
+    // public static String vnp_IpAddr = "0:0:0:0:0:0:0:1";
     public static String vnp_Locale = "vn";
     public static String vnp_ReturnUrl = "http://localhost:5173/api/vnpay/payment_infor";
 
-//    public static String vnp_ReturnUrl = "localhost/success";
+    // public static String vnp_ReturnUrl = "localhost/success";
 
-//    public static String vnp_ExpireDate = "";
+    // public static String vnp_ExpireDate = "";
 
-
-
-
-    //vpn_Ammount = tien *100;
-    //vpn_OrderInfo = Mo ta;
-    //public static String vnp_OrderType = "200000";
-    //vnp_TxnRef ma tham chieu giao dich (unique);
+    // vpn_Ammount = tien *100;
+    // vpn_OrderInfo = Mo ta;
+    // public static String vnp_OrderType = "200000";
+    // vnp_TxnRef ma tham chieu giao dich (unique);
 }
