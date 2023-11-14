75fc20c7ae325deac872bc2bfa467256e065dcec
loinq2704 <nguyenquangloi2704@gmail.com>
2023-11-14 23:12:04 +0700

    Delete news

--- src/main/java/fu/swp/dorm_mnm/controller/base/NewsController.java
+++ src/main/java/fu/swp/dorm_mnm/controller/base/NewsController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -77,6 +78,12 @@ public class NewsController {
         return ResponseEntity.status(HttpStatus.OK)
                 .contentType(MediaType.APPLICATION_PDF)
                 .body(resp);
+    }
+
+    @DeleteMapping("{newsId}")
+    public ResponseEntity<String> deleteFile(@PathVariable Long newsId){
+        newsService.remove(newsId);
+        return ResponseEntity.ok("Delete successfully!");
     }
 
 }
