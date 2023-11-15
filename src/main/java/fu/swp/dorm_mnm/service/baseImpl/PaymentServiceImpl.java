a2a5d34362b84f234a4c494e88ec793729f856d2
haikn <williamkieu2003@gmail.com>
2023-11-15 21:51:12 +0700

    final

--- src/main/java/fu/swp/dorm_mnm/service/baseImpl/PaymentServiceImpl.java
+++ src/main/java/fu/swp/dorm_mnm/service/baseImpl/PaymentServiceImpl.java
@@ -132,7 +132,7 @@ public class PaymentServiceImpl implements PaymentService {
         if (payOptional.isPresent() && payOptional.isPresent()) {
 
             Payment pay = payOptional.get();
-            if (pay.getExpirationDate().after(sqlNow)) // check expiration date
+            if (pay.getExpirationDate().before(sqlNow)) // check expiration date
                 return null;
             pay.setManager(managerOptional.get());
             pay.setStatus("not paid");
